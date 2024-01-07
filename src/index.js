f7f4ec9c5356c94f69dd81a94e1e01d91ebf77c2
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-01-07 13:18:37 +0300

    add redux store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./reset.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
