1570f83fcd512fe8f94d3557835d0c3895cfb3c9
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-01-07 13:26:56 +0300

    add defaultState

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,15 @@
 export const reducer = (state, action) => {
+  const defaultState = {
+    allCars: [],
+    raceCars: [],
+  };
+
+  if (!localStorage.getItem("race")) {
+    localStorage.setItem("race", JSON.stringify(defaultState));
+  }
+
+  state = JSON.parse(localStorage.getItem("race"));
+
   switch (action.type) {
     default:
       return state;
