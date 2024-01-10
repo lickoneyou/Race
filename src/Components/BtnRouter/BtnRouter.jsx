def3764eaab7d16d484c4ddb7631e4205e49b6ec
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-01-10 14:22:01 +0300

    del console.log

--- src/Components/BtnRouter/BtnRouter.jsx
+++ src/Components/BtnRouter/BtnRouter.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import styles from "./BtnRouter.module.css";
 
 const BtnRouter = ({ href, name, disabled }) => {
-  console.log(disabled);
-
   return (
     <form action={href}>
       <button className={styles.btn} disabled={disabled}>
