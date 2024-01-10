184f43955ab9a6d23065ffd29d13a344eb13606a
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-01-10 19:04:47 +0300

    add score

--- src/Components/Main/MainScore/MainScore.jsx
+++ src/Components/Main/MainScore/MainScore.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Score from "../../Score/Score";
 
 const MainScore = () => {
-  return <main>MainScore</main>;
+  return (
+    <main style={{ padding: "0px 300px 0px 300px" }}>
+      <Score />
+    </main>
+  );
 };
 
 export default MainScore;
---
+++ src/Components/Score/Score.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Score.module.css";
+import { useSelector } from "react-redux";
+
+const Score = () => {
+  const winners = [...useSelector((state) => state.race.allCars)].sort(
+    (a, b) => b.wins - a.wins
+  );
+  const winnerPlace = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <div className={styles.scoreContainer}>
+      <div className={[styles.row, styles.rowHead].join("")}>
+        <div className={[styles.col, styles.colHead].join("")}>Place</div>
+        <div className={[styles.col, styles.colHead].join("")}>Name</div>
+        <div className={[styles.col, styles.colHead].join("")}>Best Time</div>
+        <div className={[styles.col, styles.colHead].join("")}>Wins</div>
+      </div>
+      {winnerPlace.map((el, index) => (
+        <div className={styles.row} key={el}>
+          <div className={styles.col}>{el}</div>
+          <div className={styles.col}>{winners?.[index]?.name ?? ""}</div>
+          <div className={styles.col}>{winners?.[index]?.bestTime ?? ""}</div>
+          <div className={styles.col}>{winners?.[index]?.wins ?? ""}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Score;
